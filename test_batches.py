c7f9a47d6008ac942252ca87a7a418674b5f992f
asheikh <asim.sardar.sheikh@gmail.com>
2022-08-15 11:05:08 +1000

    Adding helper function for new tests

--- test_batches.py
+++ test_batches.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+from datetime import date
+
 from model import Batch, OrderLine
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
@@ -7,4 +10,10 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+
+def make_batch_and_line(sku: str, batch_qty: int, line_qty) -> Tuple[Batch, OrderLine]:
+    return ( Batch("batch-001", sku=sku, qty=batch_qty, eta=date.today()),
+             OrderLine(orderid="order-123", sku=sku, qty=line_qty))
+
 
